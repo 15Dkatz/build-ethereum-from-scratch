1b366d4eefe7325616589f6670851906f74fdda4
David Katz Recording <david@Davids-MacBook-Pro.local>
2019-08-17 09:19:42 -0700

    Initialize accounts

---
+++ account/index.js
@@ -0,0 +1,22 @@
+const { ec, keccakHash } = require('../util');
+const { STARTING_BALANCE } = require('../config');
+
+class Account {
+  constructor() {
+    this.keyPair = ec.genKeyPair();
+    this.address = this.keyPair.getPublic().encode('hex');
+    this.balance = STARTING_BALANCE;
+  }
+
+  sign(data) {
+    return this.keyPair.sign(keccakHash(data));
+  }
+
+  static verifySignature({ publicKey, data, signature }) {
+    const keyFromPublic = ec.keyFromPublic(publicKey, 'hex');
+
+    return keyFromPublic.verify(keccakHash(data), signature);
+  }
+}
+
+module.exports = Account;
--- util/index.js
+++ util/index.js
@@ -1,4 +1,7 @@
 const keccak256 = require('js-sha3').keccak256;
+const EC = require('elliptic').ec;
+
+const ec = new EC('secp256k1');
 
 const sortCharacters = data => {
   return JSON.stringify(data).split('').sort().join('');
@@ -14,5 +17,6 @@ const keccakHash = data => {
 
 module.exports = {
   sortCharacters,
-  keccakHash
+  keccakHash,
+  ec
 };
